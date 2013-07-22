46d4604bc54231cc1b84f531eccf21c9e08b6bd7
Ondrej Slinták <ondrowan@gmail.com>
2013-07-22 15:26:02 +0200

    levels in DB model are now 1-based

--- brabeion/base.py
+++ brabeion/base.py
@@ -63,17 +63,14 @@ class Badge(object):
         if awarded.level is None:
             assert len(self.levels) == 1
             awarded.level = 1
-        # awarded levels are 1 indexed, for conveineince
-        awarded = awarded.level - 1
-        assert awarded < len(self.levels)
         if (not self.multiple and
-            BadgeAward.objects.filter(user__pk=user_id, slug=self.slug, level=awarded)):
+            BadgeAward.objects.filter(user__pk=user_id, slug=self.slug, level=awarded.level)):
             return
         extra_kwargs = {}
         if force_timestamp is not None:
             extra_kwargs["awarded_at"] = force_timestamp
         badge = BadgeAward.objects.create(user_id=user_id, slug=self.slug,
-            level=awarded, **extra_kwargs)
+            level=awarded.level, **extra_kwargs)
         badge_awarded.send(sender=self, badge_award=badge)
     
     def freeze(self, **state):
